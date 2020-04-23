e090564304a74d70e25cb340f92f3365f86a849a
Patryk Wychowaniec <wychowaniec.patryk@gmail.com>
2020-04-23 19:30:21 +0200

    ci: Start configuring

--- cli/default.nix
+++ cli/default.nix
@@ -8,7 +8,7 @@ in
     name = "stdout";
 
     src = nix-gitignore.gitignoreSource [] ./.;
-    cargoSha256 = "1k98skj1p4bvkrcxm117a8a0w2fjwwk8iazhrm1vajaljy2106mw";
+    cargoSha256 = "0lcv7anaq1vicsp71s66bpgpmjvk5az61c2dk4962w1d0axq3zw6";
     buildType = "debug"; # @todo
 
     # Required for the tests to run
