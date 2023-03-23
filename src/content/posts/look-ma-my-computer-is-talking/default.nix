9325d6a92fad83522144247daa42234c0a9c35ca
Patryk Wychowaniec <pwychowaniec@pm.me>
2023-03-23 19:40:52 +0100

    look-ma-my-computer-is-talking: Initial commit

---
+++ src/content/posts/look-ma-my-computer-is-talking/default.nix
@@ -0,0 +1,8 @@
+{
+  title = "Look Ma: My computer is talking; let's create a simplified ChatGPT!";
+  tags = [ "ai" "shorts" "rust" ];
+  publishedAt = { y = 2023; m = 3; d = 23; };
+
+  body = ./body.html;
+  summary = ./summary.html;
+}
