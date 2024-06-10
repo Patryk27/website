e2f11b939fbf6984816e0d5366cd4484073acff9
Patryk Wychowaniec <pwychowaniec@pm.me>
2024-06-10 16:38:44 +0200

    talks: Add 2024-coolest-algorithms-and-optimizations

---
+++ src/content/talks/2024-coolest-algorithms-and-optimizations.nix
@@ -0,0 +1,7 @@
+{
+  title = "Coolest algorithms and optimizations I've used at work";
+  tags = [ "rust" "algorithms" "performance" ];
+  when = { y = 2024; m = 6; d = 6; };
+  where = "Rust Wrocław";
+  link = "https://youtu.be/S5F1cc1S4cU?t=4642";
+}
