fdfbe73cfd38cdcc9add1cd7785b5e9f3ba5702c
Patryk Wychowaniec <pwychowaniec@pm.me>
2024-08-21 16:49:49 +0200

    sak: Fix headers

--- src/utils/sak/src/cmds/compile_post/printer/process_header.rs
+++ src/utils/sak/src/cmds/compile_post/printer/process_header.rs
@@ -7,20 +7,21 @@ impl Printer<'_> {
         mut el: Element,
     ) -> MessageResult<()> {
         let id = el.remove_attr("id")?;
+        let id = id.value()?;
 
         el.check_no_more_attrs()?;
 
         self.process_generic(Element {
             name: Spanned::dummy("h2".into()),
-            attrs: Default::default(),
+            attrs: vec![Attr {
+                name: Spanned::dummy("id".into()),
+                value: Some(id.clone()),
+            }],
             children: vec![Node::Element(Spanned::dummy(Element {
                 name: Spanned::dummy("a".into()),
                 attrs: vec![Attr {
                     name: Spanned::dummy("href".into()),
-                    value: Some(Spanned::dummy(format!(
-                        "#{}",
-                        id.value()?.as_str()
-                    ))),
+                    value: Some(Spanned::dummy(format!("#{}", id.as_str()))),
                 }],
                 children: mem::take(&mut el.children),
             }))],
