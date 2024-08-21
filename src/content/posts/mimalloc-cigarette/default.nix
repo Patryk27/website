32c50f41a6e557e30f38bc5efb3a77485b6365bc
Patryk Wychowaniec <pwychowaniec@pm.me>
2024-08-21 16:42:03 +0200

    mimalloc-cigarette: Create

---
+++ src/content/posts/mimalloc-cigarette/default.nix
@@ -0,0 +1,8 @@
+{
+  title = "Mimalloc Cigarette: Losing one week of my life catching a memory leak";
+  tags = [ "memory" "mimalloc" "rust" "shorts" ];
+  publishedAt = { y = 2024; m = 8; d = 21; };
+
+  body = ./body.html;
+  summary = ./summary.html;
+}
