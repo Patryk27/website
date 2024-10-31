c359b8886dbd4575584a50be707963568fa27dfb
Patryk Wychowaniec <pwychowaniec@pm.me>
2024-10-31 16:48:53 +0100

    2024-nix-as-static-site-generator: Add

---
+++ src/content/talks/2024-nix-as-static-site-generator.nix
@@ -0,0 +1,19 @@
+{
+  title = "Nix as Static Site Generator for My Blog";
+  tags = [ "cursed" "nix" ];
+  when = { y = 2024; m = 10; d = 26; };
+  where = "NixCon 2024";
+  link = "https://youtu.be/_7wqXN-7ebw?t=6695";
+
+  resources = [
+    { label = "code"; link = "https://github.com/Patryk27/website"; }
+  ];
+
+  description = ''
+    <p>
+      Since a Nix derivation can be anything, from a text file up to a directory
+      tree, then what's stopping us from implementing a static site generator
+      using Nix? Certainly not the police!
+    </p>
+  '';
+}
